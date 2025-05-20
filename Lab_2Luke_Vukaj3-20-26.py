4f0547df631241e13ac9c64e8119f6a2fe7212c6
lukevukaj <lukevukaj@gmail.com>
2025-05-20 19:35:14 -0400

    Add files via upload

---
+++ Lab_2Luke_Vukaj3-20-26.py
@@ -0,0 +1,15 @@
+# This program asks the user for their favorite color and age, then prints out the responses.
+# Step 1: Ask the user for their favorite color
+print('What is your favorite color?')
+# Step 2: Get the user's favorite color
+# and store it in a variable
+color = input()
+# Step 3: Print a message using the variable color
+print('Your favorite color is ' + color + '.')
+# Step 4: Ask the user for their age
+print('How old are you?')
+# Step 5: Get the user's age
+# and store it in a variable
+age = input()
+# Step 6: Print a message using the variable age
+print('You are ' + age + ' years old.')
